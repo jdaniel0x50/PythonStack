b815a3e409160063a8501e56c7a843a74f3cdb01
Joshua Daniel <joshuadaniel@fuller.edu>
2017-10-02 18:33:49 -0500

    practiced turtle assignment

--- Fundamentals/py_10turtle_drawing.py
+++ Fundamentals/py_10turtle_drawing.py
@@ -1,13 +1,13 @@
 import turtle
 # the distance we want the pointer to travel each time
-DIST = 100
-for x in range(0,6):
+DIST = 10
+for x in range(0,40):
     print "x", x
     for y in range(1,5):
         print "y", y
         # turn the pointer 90 degrees to the right
-        turtle.right(90)
+        turtle.right(45)
         # advance according to set distance
         turtle.forward(DIST)
     # add to set distance
-    DIST += 20
+    DIST += 5
