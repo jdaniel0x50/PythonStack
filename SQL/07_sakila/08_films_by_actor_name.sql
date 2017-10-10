5bcb9fca18b9e596b0217be9bf49d1ca4552cc12
Joshua Daniel <joshuadaniel@fuller.edu>
2017-10-09 23:43:28 -0500

    updated SQL queries for correction

--- SQL/07_sakila/08_films_by_actor_name.sql
+++ SQL/07_sakila/08_films_by_actor_name.sql
@@ -4,5 +4,5 @@ JOIN film_actor ON actor.actor_id = film_actor.actor_id
 JOIN film ON film_actor.film_id = film.film_id
 JOIN film_category ON film.film_id = film_category.film_id
 JOIN category ON film_category.category_id = category.category_id
-WHERE actor.first_name = "SANDRA" AND actor.last_name = "KILMER"
+WHERE actor.first_name = "SANDRA" AND actor.last_name = "KILMER" AND category.name = "action"
 ORDER BY film.title ASC;
